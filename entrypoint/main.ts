782cd4b74ff5db8956596d7c2b0aef83d1b0e0be
m-haketa <git@excelspeedup.com>
2021-09-12 20:38:37 +0900

    for debug

---
+++ entrypoint/main.ts
@@ -0,0 +1,34 @@
+import * as fs from "fs";
+
+import { CodeGenerator } from "../src/";
+import * as Templates from "../src/templates";
+import type * as Types from "../src/types";
+
+const main = () => {
+  const codeGenerator = new CodeGenerator("./entrypoint/api-schema.yml");
+
+  const apiClientGeneratorTemplate: Types.CodeGenerator.CustomGenerator<Templates.ApiClient.Option> = {
+    generator: Templates.ApiClient.generator,
+    option: {},
+  };
+
+  const typeDefCode = codeGenerator.generateTypeDefinition();
+  const apiClientCode = codeGenerator.generateCode([
+    {
+      generator: () => {
+        return [`import { Schemas } from "./types";`];
+      },
+    },
+    codeGenerator.getAdditionalTypeDefinitionCustomCodeGenerator(),
+    apiClientGeneratorTemplate,
+  ]);
+
+  fs.writeFileSync("./entrypoint/__generated__/types.ts", typeDefCode, {
+    encoding: "utf-8",
+  });
+  fs.writeFileSync("./entrypoint/__generated__/apiClient.ts", apiClientCode, {
+    encoding: "utf-8",
+  });
+};
+
+main();
