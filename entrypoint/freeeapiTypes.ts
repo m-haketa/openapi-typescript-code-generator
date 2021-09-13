b393b97b74880afbceea8d4d675f5bd77bdb0e01
m-haketa <git@excelspeedup.com>
2021-09-13 15:43:19 +0900

    uriとmethodの順番を入れ替え

--- entrypoint/freeeapiTypes.ts
+++ entrypoint/freeeapiTypes.ts
@@ -1,12 +1,12 @@
 import { ParametersAndResponse as PR} from './__generated__/apiClient';
 
-type Uris = PR[keyof PR]['requestUri'];
+type Methods = PR[keyof PR]['method'];
 
-type Methods<U extends Uris> = {
-  [K in keyof PR]: PR[K]['requestUri'] extends U ? PR[K]['method'] : never;
+type Uris<M extends Methods> = {
+  [K in keyof PR]: PR[K]['method'] extends M ? PR[K]['requestUri'] : never;
 }[keyof PR]
 
-type Params<U extends Uris, M extends Methods<U>> = {
+type Params<M extends Methods, U extends Uris<M>> = {
   [K in keyof PR]: 
     PR[K]['requestUri'] extends U
     ? PR[K]['method'] extends M 
@@ -15,7 +15,7 @@ type Params<U extends Uris, M extends Methods<U>> = {
     : never;
 }[keyof PR]
 
-type Response<U extends Uris, M extends Methods<U>> = {
+type Response<M extends Methods, U extends Uris<M>> = {
   [K in keyof PR]: 
     PR[K]['requestUri'] extends U
     ? PR[K]['method'] extends M 
@@ -24,16 +24,39 @@ type Response<U extends Uris, M extends Methods<U>> = {
     : never;
 }[keyof PR]
 
-type PS = Params<'/api/1/account_items','get'>;
+type PS = Params<'get','/api/1/account_items'>;
 
-const api = <U extends Uris>(uri: U) => ({
-  method: <M extends Methods<U>>(method: M) => ({
-    parameters: <P extends Params<U, M>, R extends Response<U, M>>(params: P): R => {
+/**
+ *
+ *
+ * @template M extends Methods
+ * @param {M} method
+ */
+const freeeApi = <M extends Methods>(method: M) => ({
+/**
+ *
+ *
+ * @template U extends Uris<M>
+ * @param {U} uri
+ */
+  requestUri: <U extends Uris<M>>(uri: U) => ({
+   /**
+    *
+    *
+    * @template P extends Params<M, U>
+    * @template R extends Response<M, U>
+    * @param {P} params
+    * @returns {R}
+    */
+    parameters: <P extends Params<M, U>, R extends Response<M, U>>(params: P): R => {
       return {} as any;
     }
   })
 })
 
-const r = api('/api/1/account_items').method('get').parameters({
-  company_id: 1
-});
+const res = 
+  freeeApi('get').
+  requestUri('/api/1/account_items').
+  parameters({
+    company_id: 1,
+  });
